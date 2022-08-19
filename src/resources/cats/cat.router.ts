29faf0ededa4e9d0991fba36de778d6abc27933f
4l3x <alexander.auermaa@gmail.com>
2022-08-19 13:13:32 +0200

    Id added to URL

--- src/resources/cats/cat.router.ts
+++ src/resources/cats/cat.router.ts
@@ -2,11 +2,11 @@ import express from "express";
 import { deleteAllObjects, deleteObject, editObject, getAllObjects, getObject, postObject } from "./cat.controller";
 
 const catRouter = express.Router();
-catRouter.get("/:id", getObject);
+catRouter.get("/id/:id", getObject);
 catRouter.get("/", getAllObjects);
 catRouter.post("/", postObject);
 catRouter.put("/", editObject);
-catRouter.delete("/:id", deleteObject);
+catRouter.delete("/id/:id", deleteObject);
 catRouter.delete("/", deleteAllObjects);
 
 export default catRouter;
