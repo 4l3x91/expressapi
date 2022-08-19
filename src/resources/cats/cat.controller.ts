25b777096da71c0d056d23dee6bd7e2d692a2959
4l3x <alexander.auermaa@gmail.com>
2022-08-19 19:23:38 +0200

    Thanks, Prettier

--- src/resources/cats/cat.controller.ts
+++ src/resources/cats/cat.controller.ts
@@ -3,6 +3,9 @@ import { cats, dbPath, jsonReader, saveToFile } from "../data/data.handler";
 import { Cat, catSchema } from "./cat.model";
 import catRouter from "./cat.router";
 
+// Only works if it's here(?)
+jsonReader(dbPath);
+
 export const getObject = (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
   const cat = cats.find((x) => x.id === id);
@@ -13,8 +16,13 @@ export const getObject = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-// Only works if it's here(?)
-jsonReader(dbPath);
+export const getRandomObject = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+// TODO
+};
 
 export const getAllObjects = (
   req: Request,
@@ -57,66 +65,75 @@ export const validateObject = (
 };
 
 export const postObject = (req: Request, res: Response, next: NextFunction) => {
-    let cat: Cat = {
-      weight: req.body.weight + "kg",
-      id: req.body.name[0].toLowerCase() + "-test",
-      name: req.body.name,
-      temperament: req.body.temperament,
-      origin: req.body.origin,
-      description: req.body.description,
-      life_span: req.body.life_span,
-      indoor: req.body.indoor,
-      lap: req.body.lap,
-      adaptability: req.body.adaptability,
-      affection_level: req.body.affection_level,
-      child_friendly: req.body.child_friendly,
-      cat_friendly: req.body.cat_friendly,
-      dog_friendly: req.body.dog_friendly,
-      energy_level: req.body.energy_level,
-      grooming: req.body.grooming,
-      health_issues: req.body.health_issues,
-      intelligence: req.body.intelligence,
-      shedding_level: req.body.shedding_level,
-      social_needs: req.body.social_needs,
-      stranger_friendly: req.body.stranger_friendly,
-      vocalisation: req.body.vocalisation,
-      experimental: req.body.experimental,
-      hairless: req.body.hairless,
-      natural: req.body.natural,
-      rare: req.body.rare,
-      rex: req.body.rex,
-      suppressed_tail: req.body.suppressed_tail,
-      short_legs: req.body.short_legs,
-      hypoallergenic: req.body.hypoallergenic,
-      image: req.body.image
-    };
-    
-    cats.push(cat);
-    saveToFile(cats);
-    console.log("Cat posted successfully!");
-    res.status(200).json(cat);
-
-    next();
+  let cat: Cat = {
+    weight: req.body.weight + "kg",
+    id: req.body.name[0].toLowerCase() + "-test",
+    name: req.body.name,
+    temperament: req.body.temperament,
+    origin: req.body.origin,
+    description: req.body.description,
+    life_span: req.body.life_span,
+    indoor: req.body.indoor,
+    lap: req.body.lap,
+    adaptability: req.body.adaptability,
+    affection_level: req.body.affection_level,
+    child_friendly: req.body.child_friendly,
+    cat_friendly: req.body.cat_friendly,
+    dog_friendly: req.body.dog_friendly,
+    energy_level: req.body.energy_level,
+    grooming: req.body.grooming,
+    health_issues: req.body.health_issues,
+    intelligence: req.body.intelligence,
+    shedding_level: req.body.shedding_level,
+    social_needs: req.body.social_needs,
+    stranger_friendly: req.body.stranger_friendly,
+    vocalisation: req.body.vocalisation,
+    experimental: req.body.experimental,
+    hairless: req.body.hairless,
+    natural: req.body.natural,
+    rare: req.body.rare,
+    rex: req.body.rex,
+    suppressed_tail: req.body.suppressed_tail,
+    short_legs: req.body.short_legs,
+    hypoallergenic: req.body.hypoallergenic,
+    image: req.body.image,
   };
+
+  cats.push(cat);
+  saveToFile(cats);
+  console.log("Cat posted successfully!");
+  res.status(200).json(cat);
+
+  next();
+};
 
 export const editObject = (req: Request, res: Response, next: NextFunction) => {
   // TODO: Put object
 };
 
-export const getEndPoints = (req: Request, res: Response, next: NextFunction) => {
+export const getEndPoints = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   res.status(200).json(
     catRouter.stack
-    .filter(r => r.route)
-    .map(r => {
-      return {
-        method: Object.keys(r.route.methods)[0].toUpperCase(),
-        path: r.route.path
-      };
-    }));
-}
+      .filter((r) => r.route)
+      .map((r) => {
+        return {
+          method: Object.keys(r.route.methods)[0].toUpperCase(),
+          path: r.route.path,
+        };
+      })
+  );
+};
 
-export const validateCat = (req: Request, res: Response, next: NextFunction) => {
+export const validateCat = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const result = catSchema.validate(req.body);
-  if(result.error) res.status(400).json(result.error.message);
+  if (result.error) res.status(400).json(result.error.message);
   else next();
-}
+};
