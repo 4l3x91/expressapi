466165719318e422c6714306fa5faa78db99a3d4
4l3x <alexander.auermaa@gmail.com>
2022-08-19 12:24:28 +0200

    Cat scraper added

---
+++ src/resources/cats/cat.scraper.ts
@@ -0,0 +1,41 @@
+const fs = require("fs");
+
+export async function initCatAPI() {
+  let response = await fetch("https://api.thecatapi.com/v1/breeds");
+  let dataResult = await response.json();
+
+  await cleanUpData(dataResult);
+  await saveToFile(dataResult);
+}
+
+async function cleanUpData(data) {
+  let deletePropList = [
+    "vetstreet_url",
+    "vcahospitals_url",
+    "country_codes",
+    "country_code",
+    "cfa_url",
+    "reference_image_id",
+    "wikipedia_url",
+    "alt_names"
+  ];
+
+  for (let index = 0; index < data.length; index++) {
+    let currentObj = data[index];
+    if(currentObj.image) currentObj.image = currentObj.image.url;
+    else currentObj.image = './src/assets/img/default_cat.png';
+
+    for (let i = 0; i < deletePropList.length; i++) {
+      delete currentObj[deletePropList[i]];
+    }
+    
+    console.log(`${currentObj.name} has been cleaned!`);
+  }
+}
+
+async function saveToFile(data) {
+  await fs.writeFile("src/resources/data/catsDB.json", JSON.stringify(data, null, "\t"), (err) => {
+    if (err) console.log(err);
+    else console.log("Save done.")
+  });
+}
