3560cee5b5377180b4596e3f388d007fbd04e72e
4l3x <alexander.auermaa@gmail.com>
2022-08-19 12:27:43 +0200

    Cat model based on TheCatAPI minus deleted props added

---
+++ src/resources/cats/cat.model.ts
@@ -0,0 +1,42 @@
+import Joi from "joi";
+
+export interface Cat {
+  weight: {
+    imperial: string;
+    metric: string;
+  };
+  id: string;
+  name: string;
+  temperament: string;
+  origin: string;
+  description: string;
+  life_span: string;
+  indoor: number;
+  lap: number;
+  adaptability: number;
+  affection_level: number;
+  child_friendly: number;
+  cat_friendly: number;
+  dog_friendly: number;
+  energy_level: number;
+  grooming: number;
+  health_issues: number;
+  intelligence: number;
+  shedding_level: number;
+  social_needs: number;
+  stranger_friendly: number;
+  vocalisation: number;
+  experimental: number;
+  hairless: number;
+  natural: number;
+  rare: number;
+  rex: number;
+  suppressed_tail: number;
+  short_legs: boolean;
+  hypoallergenic: string;
+  image: string;
+}
+
+export const catSchema = Joi.object<Cat, false>({
+// TODO: Add schema
+})
