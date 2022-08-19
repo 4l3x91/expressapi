10a87d7e8c040f8f96671ce88542782b6b13a53e
4l3x <alexander.auermaa@gmail.com>
2022-08-19 21:47:38 +0200

    Cors stuff added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import catRouter from "./resources/cats/cat.router";
 import { errorHandler, logger, notFoundHandler } from "./resources/middlewares";
-
+require("express-async-errors");
+var cors = require('cors');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(logger);
 app.use("/api/cats", catRouter);
 app.use(notFoundHandler);
