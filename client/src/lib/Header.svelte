66c9ba1e1a78b4f3dbb71c6fca97ead7e30ba4b7
4l3x <alexander.auermaa@gmail.com>
2022-08-20 13:06:42 +0200

    Header added

---
+++ client/src/lib/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import catExpressLogo from "../assets/catexpress.png";
+</script>
+
+<header>
+  <img src={catExpressLogo} class="logo" alt="CatExpress Logo" />
+  <h1>CatExpress</h1>
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    background: #1c1c1c;
+    width: 100%;
+    padding: 1.5rem;
+    box-shadow: 0px 0px 30px 0px black;
+  }
+  h1 {
+    align-self: center;
+  }
+</style>
