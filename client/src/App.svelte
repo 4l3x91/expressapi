b7db8bd618b6a71c7cdb08ba4a20b5c701d2ebc4
4l3x <alexander.auermaa@gmail.com>
2022-08-20 13:06:53 +0200

    App refactored

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,40 +1,13 @@
 <script lang="ts">
-  import catExpressLogo from "./assets/catexpress.png";
   import FetchCats from "./lib/FetchCats.svelte";
+  import Footer from "./lib/Footer.svelte";
+  import Header from "./lib/Header.svelte";
 </script>
 
+<Header />
 <main>
-  <div
-    style="display: flex; justify-content: center; align-items: center; gap: 1rem;"
-  >
-    <img src={catExpressLogo} class="logo" alt="CatExpress Logo" />
-    <h1 style="align-self: center;">CatExpress</h1>
-  </div>
-
   <div class="container">
     <FetchCats />
   </div>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Footer />
