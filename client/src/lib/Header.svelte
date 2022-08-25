1058bfa79944b80c19227514352ba99604aed9ed
4l3x <alexander.auermaa@gmail.com>
2022-08-25 03:09:25 +0200

    AddBreed moved from header to main content

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-  import { getContext } from "svelte";
-
   import catExpressLogo from "../assets/catexpress.png";
-  import AddPopup from "./AddPopup.svelte";
-  const { open } = getContext("simple-modal");
 </script>
 
 <header>
   <img src={catExpressLogo} class="logo" alt="CatExpress Logo" />
   <h1>CatExpress</h1>
-  <button on:click={() => open(AddPopup, {})} class="add-breed"
-    >+ Add breed</button
-  >
+
 </header>
 
 <style>
@@ -19,7 +13,7 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    gap: 1rem;
+    gap: 2rem;
     background: #1c1c1c;
     width: 100%;
     padding: 1.5rem;
@@ -27,9 +21,5 @@
   }
   h1 {
     align-self: center;
-  }
-  .add-breed {
-    position: absolute;
-    right: 10rem;
   }
 </style>
