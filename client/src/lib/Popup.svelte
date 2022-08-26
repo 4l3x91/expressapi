44cba4c92dd1a6af9d61fdcc6b3f589e1378aeda
4l3x-suvnet <alexander.auermaa@gmail.com>
2022-08-26 10:54:51 +0200

    EditForm on already existing cat has been updated

--- client/src/lib/Popup.svelte
+++ client/src/lib/Popup.svelte
@@ -1,10 +1,230 @@
 <script lang="ts">
-  import { toast } from "@zerodevx/svelte-toast";
-  import { getContext } from "svelte";
   import { fly } from "svelte/transition";
-  import { breedArray } from "./stores";
   export let specificCat;
   let countryFlag;
+
+  import Slider from "@bulatdashiev/svelte-slider";
+  import { toast } from "@zerodevx/svelte-toast";
+  import { getContext } from "svelte";
+  import { breedArray } from "./stores";
+
+  let life_span = [5, 40];
+  let weight = [3, 35];
+
+
+  function grabRandomCatMoji() {
+    let catArray = ["🐱", "😻", "😽", "😿", "😸", "😾", "🙀", "😹", "😼", "😺"];
+    let randomCatIndex = Math.floor(Math.random() * catArray.length);
+    var randomCat = catArray[randomCatIndex];
+    return randomCat;
+  }
+
+  let traitsArray = [
+    { name: "Intelligence", propName: "intelligence", value: [1, 5] },
+    { name: "Health Issues", propName: "health_issues", value: [1, 5] },
+    { name: "Energy Level", propName: "energy_level", value: [1, 5] },
+    { name: "Adaptability", propName: "adaptability", value: [1, 5] },
+    { name: "Grooming", propName: "grooming", value: [1, 5] },
+    { name: "Affection", propName: "affection_level", value: [1, 5] },
+  ];
+
+  let flags = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "The Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "Brunei Darussalam",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cabo Verde",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "The Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombia",
+    "The Comoros",
+    "Congo",
+    "Costa Rica",
+    "Croatia",
+    "Cuba",
+    "Cyprus",
+    "Czechia",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Eswatini",
+    "Ethiopia",
+    "Fiji",
+    "Finland",
+    "France",
+    "Gabon",
+    "Gambia",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Greece",
+    "Grenada",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "North Korea",
+    "Southe Korea",
+    "Kosovo",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Micronesia",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Montenegro",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Qatar",
+    "Republic Of North Macedonia ",
+    "Romania",
+    "Russia",
+    "Rwanda",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent and the Grenadines",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "South Sudan",
+    "Suriname",
+    "Sweden",
+    "Switzerland",
+    "Syrian Arab Republic ",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "The Holy See ",
+    "The Marshall Islands",
+    "Timor-Leste ",
+    "Togo",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Venezuela",
+    "Vietnam",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+  ];
+
   let onCancel = () => {};
 
   async function getBreedById() {
@@ -219,12 +439,9 @@
             >
               WEIGHT
             </div>
-            <div>
-              {specificCat.weight}
-              {#if specificCat.weight != "N/A"}
-                <span> kg</span>
-              {/if}
-            </div>
+            
+              <div>
+                {specificCat.weight}</div>
           </div>
           <div
             style="display: flex;justify-content: space-between;text-align: right;border-top: 1px solid #b5b5b5;border-bottom: 1px solid #b5b5b5;padding: 10px;"
@@ -264,30 +481,179 @@
     out:fly={{ y: 300, duration: 500 }}
     class="modal-cat-container"
   >
-    <h1>Form</h1>
     <form
       class="content"
       on:submit|preventDefault={() => {
         setVisible();
+        editBreed();
       }}
     >
-      <div>Name</div>
+      <div class="modal-cat-container">
+        <div class="modal-cat-container d-flex">
+          <div class="modal-cat-container-left w-100">
+              <div class="breed-name-container">
+                <div class="breed-name-text">Breed name</div>
+                <div class="breed-input-container">
+                  <input
+                    class="breed-input"
+                    minlength="4"
+                    placeholder="Enter breed name"
+                    required
+                    bind:value={specificCat.name}
+                  />
+                </div>
+              </div>
+      
+              <div class="breed-origin-container">
+                <div class="breed-origin-text">Breed origin</div>
+                <div class="breed-origin-flag-container">
+                  <div class="breed-origin-flag-box">
+                    <img
+                      class="breed-origin-flag"
+                      src="https://countryflagsapi.com/png/{specificCat.origin}"
+                      alt={specificCat.origin}
+                    />
+                  </div>
+                  <div>
+                    <div>Select a country</div>
+                    <select bind:value={specificCat.origin}>
+                      {#each flags as flag}
+                        <option placeholder={flag} value={flag}>{flag}</option>
+                      {/each}
+                    </select>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="breed-traits-text">Breed traits</div>
+              <div class="traits-container">
+                {#each traitsArray as item}
+                  <div class="trait-container">
+                    <div class="trait-box">
+                      <div class="trait-text">
+                        {item.name}
+                      </div>
+                      <Slider
+                        bind:value={item.value}
+                        min="0"
+                        max="4"
+                        step="1"
+                        name={item.propName}
+                      >
+                        <span>{grabRandomCatMoji()}</span>
+                        <div class="slider-text">
+                          {item.value[0] + 1}
+                        </div>
+                      </Slider>
+                    </div>
+                  </div>
+                {/each}
+              </div>
+      
+              <div class="weight-container">
+                <div class="breed-title">Weight</div>
+                <div>
+                  <Slider
+                    bind:value={weight}
+                    max="40"
+                    range
+                    on:input={(e) => {
+                      if (weight[0] > weight[1]) weight.sort();
+                      if (weight[1] < weight[0]) weight.sort();
+                      console.log(e.detail);
+                    }}
+                  >
+                    <div slot="left">
+                      <span>{grabRandomCatMoji()}</span>
+                      <div class="slider-text">
+                        {weight[0] + 5}
+                      </div>
+                    </div>
+                    <div slot="right">
+                      <span>{grabRandomCatMoji()}</span>
+                      <div class="slider-text">
+                        {weight[1] + 5}
+                      </div>
+                    </div>
+                  </Slider>
+                </div>
+              </div>
+      
+              <div class="life-span-container">
+                <div class="breed-title">Life Span</div>
+                <div>
+                  <Slider
+                    bind:value={life_span}
+                    max="40"
+                    range
+                    on:input={(e) => {
+                      if (life_span[0] > life_span[1]) life_span.sort();
+                      if (life_span[1] < life_span[0]) life_span.sort();
+                      console.log(e.detail);
+                    }}
+                  >
+                    <div slot="left">
+                      <span>{grabRandomCatMoji()}</span>
+                      <div class="slider-text">
+                        {life_span[0] + 5}
+                      </div>
+                    </div>
+                    <div slot="right">
+                      <span>{grabRandomCatMoji()}</span>
+                      <div class="slider-text">
+                        {life_span[1] + 5}
+                      </div>
+                    </div>
+                  </Slider>
+                </div>
+              </div>
+      
+              <div class="breed-image-container">
+                <div class="breed-image-text">
+                  Image link
+                  <div style="font-size: 10px; align-self: center; display: inline;">
+                    Leave blank for default image
+                  </div>
+                </div>
+                <div class="breed-input-container">
+                  <input
+                    type="url"
+                    class="image-input w-100"
+                    placeholder="Enter image link"
+                    bind:value={specificCat.image}
+                  />
+                </div>
+              </div>
+      
+              <div class="breed-description-container">
+                <div class="breed-description-text">Breed description</div>
+                <div class="breed-input-container">
+                  <textarea
+                    required
+                    class="breed-input breed-description-input"
+                    placeholder="Enter breed description"
+                    bind:value={specificCat.description}
+                  />
+                </div>
+              </div>
+      
 
-      <input type="text" bind:value={specificCat.name} />
-      <div>Description</div>
-      <input type="text" bind:value={specificCat.description} />
-      <button
-        on:click={() => {
-          editBreed();
-          toast.push(
-            "Breed has been updated! 😻",
-            { classes: ['custom', 'success'] },
-            { pausable: true }
-          );
-        }}
-      >
-        Submit
-      </button>
+              <button
+              on:click={() => {
+                editBreed();
+                toast.push(
+                  "Breed has been updated! 😻",
+                  { classes: ['custom', 'success'] },
+                );
+              }}
+            >
+              Submit
+            </button>
+      
+            <!-- </form> -->
+          </div>
+        <!-- </div> -->
+      <!-- </div> -->
     </form>
   </div>
 {/if}
@@ -335,4 +701,141 @@
     line-height: 32px;
     cursor: pointer;
   }
+
+
+  input:focus:invalid,
+        textarea:invalid {
+          border: 1px solid red;
+        }
+      
+        textarea:valid {
+          border: 1px solid green;
+        }
+      
+        .breed-image-container {
+          padding: 20px 0;
+        }
+        form {
+          padding: 10px;
+          margin-bottom: 10px;
+        }
+        .weight-container,
+        .life-span-container {
+          padding: 10px 0;
+        }
+        .slider-text {
+          position: absolute;
+          left: 0;
+          right: 0;
+        }
+        .d-flex {
+          display: flex;
+        }
+      
+        .w-100 {
+          width: 100%;
+        }
+      
+        .breed-description-input {
+          resize: none;
+          height: 200px;
+          width: 100%;
+        }
+      
+        .breed-name-container,
+        .breed-origin-container,
+        .breed-description-container,
+        .breed-traits-text,
+        .breed-image-text {
+          text-align: left;
+        }
+      
+        .breed-description-container textarea {
+          font-size: 16px;
+          margin-bottom: 20px;
+        }
+        .breed-description-container textarea,
+        .breed-input,
+        .image-input {
+          border-radius: 0.3rem;
+          outline: none;
+          border: 1px solid #8f8f9d;
+          padding: 10px;
+        }
+      
+        .breed-name-text,
+        .breed-origin-text,
+        .breed-traits-text,
+        .breed-description-text,
+        .breed-image-text {
+          padding: 10px 0;
+          font-size: 22px;
+        }
+      
+        .breed-origin-container {
+          padding: 10px 0;
+        }
+      
+        .breed-input {
+          font-size: 20px;
+        }
+      
+        .breed-origin-text {
+          padding: 10px 0;
+        }
+      
+        .breed-image-text {
+          align-items: center;
+          display: flex;
+          gap: 1rem;
+        }
+      
+        .breed-origin-flag-container {
+          display: flex;
+          gap: 1rem;
+          height: 85px;
+          align-items: center;
+        }
+      
+        .breed-origin-flag-box {
+          display: flex;
+          align-items: center;
+        }
+      
+        .breed-origin-flag {
+          width: 80px;
+          align-content: center;
+          display: flex;
+        }
+      
+        .traits-container {
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+          gap: 2rem;
+        }
+      
+        .trait-container {
+          display: flex;
+          justify-content: space-around;
+        }
+      
+        .trait-box {
+          text-align: center;
+          width: 100%;
+        }
+      
+        .trait-text,
+        .breed-title {
+          font-size: 18px;
+        }
+      
+        .traits-container {
+          cursor: default;
+          user-select: none;
+        }
+      
+        span {
+          cursor: pointer;
+          font-size: 1.75rem;
+        }  
 </style>
